b97ff7ed859fd31bec1bc8ad43af41fd39b1bf38
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-23 16:26:15 +0900

    genres 추가(map 사용)

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-function Movie({ title, year, summary, poster }) {
+function Movie({ title, year, summary, poster, genres }) {
     return (
-        <div class="movie">
+        <div className="movie">
             <img src={poster} alt={title} title={title} />
-            <div class="movie__data">
-                <h3 class="movie__title">{title}</h3>
-                <h5 class="movie__year">{year}</h5>
-                <p class="movie__summary">{summary}</p>
+            <div className="movie__data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className='movie__genres'>
+                    {genres.map((genre, index) => {
+                        return <li className='movie__genre' key={index}>{genre}</li>;
+                    })}
+                </ul>
+                <p className="movie__summary">{summary}</p>
             </div>
         </div>    
     );
@@ -19,6 +24,8 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    //arrayOf(PropTypes.string)은 문자열을 원소로 하는 배열을 의미함.
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
