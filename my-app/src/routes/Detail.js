02d16f8f13f8c7b78890267f11b48238d17f4fc2
Rami <138556019+rami242424@users.noreply.github.com>
2023-11-24 23:27:46 +0900

    movie-detail로 바로 이동하기

--- my-app/src/routes/Detail.js
+++ my-app/src/routes/Detail.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-function Detail(props) {
-    console.log(props);
-    return <span>hello</span>;
+class Detail extends React.Component{
+    componentDidMount(){ // 1. Detail 컴포넌트가 마운트되면
+        const { location, history } = this.props; // 2. 구조 분해 할당으로 location, history를 얻고
+        
+        if (location.state === undefined) { // 3. location.state가 없는 경우
+            history.push('/'); // 4. Home으로 이동시키기
+        }
+    }
+
+    render(){
+        const { location } = this.props;
+        if (location.state){
+            return <span>{location.state.title}</span>;
+        } else {
+            return null;
+        }
+    }
 }
 
 export default Detail;
